3254eac42f9678e9a4ae46c660db92bbdd96ac70
seanbaraka <seanbaraka@gmail.com>
2024-09-22 21:07:30 +0300

    introduce a .env file to store secrets

--- _config.js
+++ _config.js
@@ -1,9 +1,9 @@
-var config = {}
+var config = {};
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+  production: process.env.MONGODB_URI,
+  development: process.env.MONGODB_URI,
+  test: process.env.MONGODB_URI,
+};
 module.exports = config;
