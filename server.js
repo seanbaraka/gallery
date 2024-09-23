8f3d8722263e12feb05861587b7ab0ce509a0f4f
seanbaraka <seanbaraka@gmail.com>
2024-09-23 10:22:14 +0300

    conflict resolution

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
-const dotenv = require("dotenv");
-dotenv.config();
-=======
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const config = require('./_config');
+const config = require("./_config");
 
->>>>>>> test
 // Define routes
 let index = require("./routes/index");
 let image = require("./routes/image");
@@ -21,41 +12,26 @@ let image = require("./routes/image");
 const app = express();
 
 // connecting the database
-<<<<<<< HEAD
-let mongodb_url = process.env.MONGODB_URI;
-let dbName = "darkroom";
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || config.mongoURI[app.settings.env];
 mongoose.connect(
-  `${mongodb_url}${dbName}`,
+  MONGODB_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
-    if (err) console.log(err);
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Connected to Database: ${MONGODB_URI}`);
+    }
   }
 );
-
-// test if the database has connected successfully
-let db = mongoose.connection;
-db.once("open", () => {
-  console.log("Database connected successfully");
-});
-=======
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
-});
 
 // test if the database has connected successfully
 // let db = mongoose.connection;
 // db.once('open', ()=>{
 //     console.log('Database connected successfully')
 // })
-
->>>>>>> test
-
 
 // View Engine
 app.set("view engine", "ejs");
@@ -70,16 +46,8 @@ app.use("/", index);
 app.use("/image", image);
 
 const PORT = process.env.PORT || 5000;
-<<<<<<< HEAD
-console.log("running on port", PORT);
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
 });
-=======
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
-});
-
 
 module.exports = app;
->>>>>>> test
